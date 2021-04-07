5546c25b008e385f3f24ed574f055c3f6aa72cad
Thone Cardoso de Araujo <thonecardoso@gmail.com>
2021-04-07 20:50:13 -0300

    create project

---
+++ src/main/java/com/example/socialbooks/SocialbooksApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialbooksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialbooksApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialbooks/SocialbooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
